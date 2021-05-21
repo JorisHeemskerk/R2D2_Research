e212064b74cde7137b8a04af9dbac8935bfbb04c
RickHoreman <rickhoreman@gmail.com>
2021-05-21 16:31:19 +0100

    Added image conversion to DataSetGenerator.py, a prototype dataset and NpyListReader.py

---
+++ dataSetGenerationTools/NpyListReader.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+setName = "prototype"
+
+with open(f'{setName}/_coordList.npy', 'rb') as f:
+    coordList = np.load(f)
+
+for i in range(len(coordList)):
+    print(f"{i}: {coordList[i]}")
