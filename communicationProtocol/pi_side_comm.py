663ba4eda3692b3bcad7c97291d7b3b24b0fc5ce
RickHoreman <rickhoreman@gmail.com>
2021-06-04 15:06:40 +0100

    It works now! Only Pi side in this commit

--- communicationProtocol/pi_side_comm.py
+++ communicationProtocol/pi_side_comm.py
@@ -1,5 +1,6 @@
 from gpiozero import InputDevice
 from gpiozero import OutputDevice
+import time
 
 transactionStart = InputDevice(11, pull_up=False)#Invert for active high or active low (for all below too)
 transactionStop = OutputDevice(8, active_high=True)
@@ -15,25 +16,46 @@ bit4 = OutputDevice(12, active_high=True)
 bit5 = OutputDevice(16, active_high=True)
 bit6 = OutputDevice(20, active_high=True)
 bit7 = OutputDevice(21, active_high=True)
-dataBus = [bit0, bit1, bit2, bit3, bit4, bit5, bit6, bit7]
+dataBus = [bit7, bit6, bit5, bit4, bit3, bit2, bit1, bit0]
+
+def turnOffOutPins():
+    transactionStop.off()
+    piReady.off()
+    bit0.off()
+    bit1.off()
+    bit2.off()
+    bit3.off()
+    bit4.off()
+    bit5.off()
+    bit6.off()
+    bit7.off()
 
 def sendData(data): #Data should be a 2D array of shape (dataAmount, len(dataBus)), containing only Bools
-    transactionStop.off()#Just to be sure
+    turnOffOutPins()
     while(not transactionStart.is_active):
+        print("Waiting for start.")
         pass
     for i in range(len(data)):
         for j in range(len(dataBus)):
             dataBus[j].value = data[i][j]
+        print("Data set")
         piReady.on()
-        while(not teensyReady.is_active and not transactionStart.is_active):
+        #time.sleep(2)
+        print("Pi ready")
+        while(not teensyReady.is_active): #and not transactionStart.is_active):
+            print("Teensy not ready")
             pass
         if(teensyReady.is_active):
             piReady.off()
-        elif(transactionStart.is_active):
-            return False
+            print("Pi not ready")
+        #elif(transactionStart.is_active):
+        #    turnOffOutPins()
+        #    return False
     transactionStop.on()
+    turnOffOutPins()
     return True
 
-data = [[0,0,0,0,0,0,0,0],[0,0,0,0,1,1,1,1],[1,1,1,1,1,1,1,1,]]
+data = [[0,0,0,0,0,0,0,0],[0,0,0,0,1,1,1,1],[1,1,1,1,1,1,1,1],[0,0,0,0,0,0,0,0],[0,0,0,0,1,1,1,1],[1,1,1,1,1,1,1,1],[0,0,0,0,0,0,0,0],[0,0,0,0,1,1,1,1],[1,1,1,1,1,1,1,1]]
 
+print("sendData return: ", end='')
 print(sendData(data))
