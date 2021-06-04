bdc53a36ff6285d5349d636f3d1823bcc781f5fa
RickHoreman <rickhoreman@gmail.com>
2021-06-04 16:22:25 +0100

    First message sent, seemingly entirely succesful, in around 12 sec

--- communicationProtocol/pi_side_comm.py
+++ communicationProtocol/pi_side_comm.py
@@ -1,6 +1,14 @@
 from gpiozero import InputDevice
 from gpiozero import OutputDevice
 import time
+from skimage import io
+import numpy as np
+import os
+import math
+
+def fractionToByte(fraction):
+    fraction = max(0.0, min(1.0, fraction))
+    return math.floor(255 if fraction == 1.0 else fraction * 256.0)
 
 transactionStart = InputDevice(11, pull_up=False)#Invert for active high or active low (for all below too)
 transactionStop = OutputDevice(8, active_high=True)
@@ -16,7 +24,7 @@ bit4 = OutputDevice(12, active_high=True)
 bit5 = OutputDevice(16, active_high=True)
 bit6 = OutputDevice(20, active_high=True)
 bit7 = OutputDevice(21, active_high=True)
-dataBus = [bit7, bit6, bit5, bit4, bit3, bit2, bit1, bit0]
+dataBus = [bit0, bit1, bit2, bit3, bit4, bit5, bit6, bit7]
 
 def turnOffOutPins():
     transactionStop.off()
@@ -33,21 +41,23 @@ def turnOffOutPins():
 def sendData(data): #Data should be a 2D array of shape (dataAmount, len(dataBus)), containing only Bools
     turnOffOutPins()
     while(not transactionStart.is_active):
-        print("Waiting for start.")
+        #print("Waiting for start.")
         pass
     for i in range(len(data)):
+        #print(data[i])
         for j in range(len(dataBus)):
-            dataBus[j].value = data[i][j]
-        print("Data set")
+            shifted = data[i] >> j
+            dataBus[j].value = 0b1 & shifted
+        #print("Data set")
         piReady.on()
         #time.sleep(2)
-        print("Pi ready")
+        #print("Pi ready")
         while(not teensyReady.is_active): #and not transactionStart.is_active):
-            print("Teensy not ready")
+            #print("Teensy not ready")
             pass
         if(teensyReady.is_active):
             piReady.off()
-            print("Pi not ready")
+            #print("Pi not ready")
         #elif(transactionStart.is_active):
         #    turnOffOutPins()
         #    return False
@@ -55,7 +65,17 @@ def sendData(data): #Data should be a 2D array of shape (dataAmount, len(dataBus
     turnOffOutPins()
     return True
 
-data = [[0,0,0,0,0,0,0,0],[0,0,0,0,1,1,1,1],[1,1,1,1,1,1,1,1],[0,0,0,0,0,0,0,0],[0,0,0,0,1,1,1,1],[1,1,1,1,1,1,1,1],[0,0,0,0,0,0,0,0],[0,0,0,0,1,1,1,1],[1,1,1,1,1,1,1,1]]
+data = []
+
+filename = os.path.join("images/blackDark_corrected/" + "0" + ".jpg")
+image = io.imread(filename)
+
+for row in image:
+    for pixel in row:
+        for part in pixel:
+            data.append(part)
+
+#data = [0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xAA,0xBB,0xCC,0xDD,0xEE,0xFF]
 
 print("sendData return: ", end='')
 print(sendData(data))
