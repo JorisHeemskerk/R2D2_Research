9e23515a314b0dd09411e8412285c7e9f9e93bec
Rick Horeman <rickhoreman@gmail.com>
2021-06-14 13:38:07 +0200

    More result processing stuff and more results

--- communicationProtocol/results/processResults.py
+++ communicationProtocol/results/processResults.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 import os
 import binascii
 from skimage import io
+import math
 
 from scatterGraph import scatter_graph
 
@@ -60,29 +61,73 @@ def load_results_dict(filename):
                     bigArr.append(tempArr)
                     tempNum = ""
                     tempArr = []
+    resultsDict[prevChecksum] = bigArr
                 
     #for key, value in resultsDict.items() :
     #    print(key)
     return resultsDict
 
-for i in range(130):
-    setName = 'blackDark_corrected'
-    index = i
+def averageHSV(hsvData):
+    H, S, V = 0, 0, 0
+    for data in hsvData:
+        H += data[0]
+        S += data[1]
+        V += data[2]
+    return (H/len(hsvData), S/len(hsvData), V/len(hsvData))
 
-    print('Loading checksums')
-    checksums = load_checksums_from_images(f'images/{setName}/')
-    print('Finished loading checksums')
+def find_best_and_worst_hsv_scales(guesses, laserLocation, scaleStart=0, scaleEnd=10):
+    #a tuple containing (Hscale, Sscale, Vscale, distance)
+    best = (0,0,0,float('inf'))
+    worst = (0,0,0,-1)
 
-    print('Loading coordlist')
-    with open(f'images/{setName}/_coordList.npy', 'rb') as f:
+    #The testing code runs three for loops (one inside the other, etc.) Going through all combinations of H, S and V scales, so we do the same thing here.
+    for h in range(scaleStart, scaleEnd):
+        for s in range(scaleStart, scaleEnd):
+            for v in range(scaleStart, scaleEnd):
+                distance = math.sqrt((laserLocation[0] - guesses[(h*100)+(s*10)+v][0])**2 + (laserLocation[1] - guesses[(h*100)+(s*10)+v][1])**2)
+                if distance < best[3]:
+                    best = (h,s,v,distance)
+                if distance > worst[3]:
+                    worst = (h,s,v,distance)
+    return best, worst
+
+def find_avg_best_and_worst_hsv_scales(resultsDict, checksums, coordList):
+    best_values = []
+    worst_values = []
+    for i in range(len(coordList)):
+        data = np.array(resultsDict[checksums[i]])
+        best, worst = find_best_and_worst_hsv_scales(data, coordList[i])
+        best_values.append(best)
+        worst_values.append(worst)
+    avg_best = averageHSV(best_values)
+    avg_worst = averageHSV(worst_values)
+    return avg_best, avg_worst
+
+
+
+setNames = ['blackDark', 'blackLight', 'greyDark', 'greyLight', 'whiteDark', 'whiteLight', 'mirror', 'r3d']
+dbg = False
+for setName in setNames:
+    index = 0
+
+    if dbg: print('Loading checksums')
+    checksums = load_checksums_from_images(f'images/{setName}_corrected/')
+    if dbg: print('Finished loading checksums')
+
+    if dbg: print('Loading coordlist')
+    with open(f'images/{setName}_corrected/_coordList.npy', 'rb') as f:
         coordList = np.load(f)
-    print('Finished loading coordlist')
+    if dbg: print('Finished loading coordlist')
 
-    print('Loading results')
-    resultsDict = load_results_dict(f'output/output_{setName}.txt')
-    print('Finished loading results')
+    if dbg: print('Loading results')
+    resultsDict = load_results_dict(f'output/output_{setName}_corrected.txt')
+    if dbg: print('Finished loading results')
 
-    print('Processing results')
+    if dbg: print('Processing results')
     data = np.array(resultsDict[checksums[index]])
 
-    scatter_graph(data, coordList[index])
+    #scatter_graph(data, coordList[index])
+
+    bestHSV, worstHSV = find_avg_best_and_worst_hsv_scales(resultsDict, checksums, coordList)
+    print(f'For {setName} the best HSV values are {bestHSV} and the worst HSV values are {worstHSV}')
+
